c7816c803429f3e9dcb3274c78bfb396152fc3ea
mateus-clasen <mateushc2@hotmail.com>
2024-04-17 18:16:32 -0300

    notes boxes

--- app/src/main/java/com/clasenmateus/notes_app/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/clasenmateus/notes_app/feature_note/domain/util/NoteOrder.kt
@@ -4,8 +4,15 @@ package com.clasenmateus.notes_app.feature_note.domain.util
  * Created by Mateus H. Clasen on 16/04/2024.
  */
 sealed class NoteOrder(val orderType: OrderType) {
+    class Title(orderType: OrderType) : NoteOrder(orderType)
+    class Date(orderType: OrderType) : NoteOrder(orderType)
+    class Color(orderType: OrderType) : NoteOrder(orderType)
 
-    class Title(orderType: OrderType): NoteOrder(orderType)
-    class Date(orderType: OrderType): NoteOrder(orderType)
-    class Color(orderType: OrderType): NoteOrder(orderType)
+    fun copy(orderType: OrderType): NoteOrder {
+        return when (this) {
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+            is Color -> Color(orderType)
+        }
+    }
 }
